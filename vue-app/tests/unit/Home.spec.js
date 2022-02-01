f8f1954497fa45b336da482d736c8a8fbe6cf2a6
mbakin <mehmetberkakinn@outlook.com>
2022-02-01 19:45:09 +0300

    unit testing red

---
+++ vue-app/tests/unit/Home.spec.js
@@ -0,0 +1,27 @@
+import {mount} from "@vue/test-utils"
+import Home from "../../src/views/Home"
+import CardVideo from "../../src/components/CardVideo"
+
+describe("Home.vue", () => {
+  test("Rendered correctly check", () => {
+    const getters = {
+      getVideos: [
+        {title: "title1"},
+        {title: "title2"},
+      ]
+    }
+    const $store = {
+      getters,
+    }
+    
+    const wrapper = mount(Home, {
+      mocks: {
+        $store,
+      }
+    })
+    expect(wrapper.exists()).toBeTruthy()
+    
+    const cardVideoComponents = wrapper.findAllComponents(CardVideo)
+    expect(cardVideoComponents.length).toBe(2)
+  })
+})
