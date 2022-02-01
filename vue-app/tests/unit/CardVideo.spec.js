8c229920a2e64edc5634f38051bdd37c881e3c88
mbakin <mehmetberkakinn@outlook.com>
2022-02-01 19:20:21 +0300

    unit test failed

--- vue-app/tests/unit/CardVideo.spec.js
+++ vue-app/tests/unit/CardVideo.spec.js
@@ -2,7 +2,18 @@ import { shallowMount } from "@vue/test-utils";
 import CardVideo from "../../src/components/CardVideo.vue";
 
 describe("CardVideo.vue", () => {
-  test("CardVideo is exist correctly", () => {
-    expect(CardVideo).toBeTruthy();
+  test("Rendered correctly check", () => {
+    const wrapper = shallowMount(CardVideo, {
+      propsData: {
+        video: {}
+      }
+    })
+    expect(wrapper.exists()).toBeTruthy()
+    
+    const card = wrapper.find("#card-video")
+    expect(card.exists()).toBeTruthy()
+    
+    const image = wrapper.find("img")
+    expect(image.exists()).toBeTruthy()
   })
 })
